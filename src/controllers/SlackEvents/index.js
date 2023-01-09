137a96d8cd098caed496bf40c07bafaa8ad32ca9
fedeq <federicoquattrocchio@gmail.com>
2023-01-09 13:42:49 -0300

    Connect with openAI API

--- src/controllers/SlackEvents/index.js
+++ src/controllers/SlackEvents/index.js
@@ -1,4 +1,5 @@
 const { sendMessage } = require("../../lib/slack/index");
+const { runCompletion } = require("../../lib/openai/index")
 
 module.exports.postEvent = async (req, res) => {
   if (req.body.type === "url_verification") {
@@ -6,10 +7,14 @@ module.exports.postEvent = async (req, res) => {
     return;
   }
 
-  const { channel, type, test, user, ts, blocks, team } = req.body.event;
+  const { channel, type, test, user, ts, blocks, team, text } = req.body.event;
 
   try {
-    await sendMessage(channel, `Hi <@${user}> ! This is Haly!`);
+    let prompt = "The following is a conversation between a Slack user and a Slack bot named Haly. Haly can do things like answer questions, create a thread summary, schedule a meeting."
+    const userMessage = text.replace("<@U04HV520KQE>","USER: ").trim();
+    prompt = prompt + userMessage + "\nHaly: ";
+    const response = await runCompletion(prompt);
+    await sendMessage(channel, response);
   } catch (error) {
     console.error(error);
   }
--- src/lib/openai/index.js
+++ src/lib/openai/index.js
@@ -1,16 +1,17 @@
 const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-(async () => {
-  const configuration = new Configuration({
-    organization: "org-eZs5fyx4D8tB4TDuGoGvcFot",
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-
+const runCompletion = async(text) => {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: "Hello world",
+    model: "text-curie-001",
+    prompt: text,
+    max_tokens: 1000,
+    temperature: 0.7
   });
-  console.log(completion.data);
-  console.log(completion.data.choices[0].text);
-})();
+  return completion.data.choices[0].text;
+}
+
+module.exports = { runCompletion };
