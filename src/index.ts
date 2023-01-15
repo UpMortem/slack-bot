8e1a2958fce5bd436d9600157edbfd75eec28df8
andre-labelbox <abrongniart@labelbox.com>
2023-01-14 22:59:10 -0500

    cloud run debug

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const serverDebug = debug("haly:server");
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.HALY_PORT || "3000");
 app.set("port", port);
 
 /**
