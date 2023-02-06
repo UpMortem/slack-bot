15273209a1463ab5fd2b22d9c529e9018bb20d0d
Joel Bandi <joelvivekbandi@gmail.com>
2023-02-06 00:54:21 -0800

    dont respond to yourself haly
    also move slack code to slack service

--- src/services/slack_service.py
+++ src/services/slack_service.py
@@ -1,10 +1,11 @@
 from slack_bolt import App
+import time
 import os
+from services.openai_service import respond_to_user
 
 # grabs the credentials from .env directly
 slack_app = App()
 users_map = {}
-
 
 def send_message(channel: str, thread_ts: str, text: str):
     try:
@@ -54,3 +55,57 @@ def get_username(user_id: str):
         user = find_user_by_id(user_id)
         users_map[user_id] = user["user"]["name"]
     return users_map[user_id]
+
+def find_bot_id(payload):
+    for auth in payload["authorizations"]:
+        # Check if the current authorization is a bot
+        if auth["is_bot"]:
+            return auth["user_id"]
+
+    return None
+
+def get_sender(payload):
+    return payload.get("event").get("user")
+
+
+def process_event_payload(payload):
+    sender = get_sender(payload)
+    if sender is None:
+        print("sender not found")
+        return
+
+    bot_id = find_bot_id(payload)
+    if not bot_id:
+        print("botId not found")
+        return
+
+    if sender == bot_id:
+        return
+
+    event = payload.get("event")
+    channel = event.get("channel")
+    text = event.get("text")
+    thread_ts = event.get("thread_ts")
+    ts = event.get("ts")
+
+    try:
+        thread_to_reply = thread_ts
+        if thread_ts != ts:
+            thread_to_reply = ts
+        messages = f"USER: {text.replace(f'<@{bot_id}>','').strip()}"
+        if thread_ts:
+            messages = (
+                get_thread_messages_with_usernames(channel, thread_ts, bot_id)
+                or messages
+            )
+
+        start_time = time.perf_counter()
+        response = respond_to_user(messages)
+        end_time = time.perf_counter()
+        print(f"response generated in {round(end_time - start_time, 2)}s")
+
+        return send_message(channel, thread_to_reply, response)
+    except Exception as error:
+        # Improve error handling
+        print(error)
+        return
