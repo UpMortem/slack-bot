a36dbeb9178f0d19b66b0cd4c982e69a6d996874
Navraj Chohan <nlake44@gmail.com>
2023-02-16 15:05:13 -0800

    Make Haly an expert in all things

--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -3,11 +3,11 @@ import os
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-base_prompt = "You are a Slack bot named Haly. You are having a conversation with a user.\n\n"
+base_prompt = "You are a Slack bot named Haly. You are an expert in everything. You are a consultant. You are having a conversation with a client.\n\n"
 
 def run_completion(text):
     completion = openai.Completion.create(
-        model="text-davinci-003", prompt=text, max_tokens=1000, temperature=0.7
+        model="text-davinci-003", prompt=text, max_tokens=3000, temperature=0.7
     )
     return completion.choices[0].text
 
