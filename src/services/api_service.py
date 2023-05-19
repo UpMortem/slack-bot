82182a867867ade1533e3242832a0fb46243ae6b
fedeq <federicoquattrocchio@gmail.com>
2023-05-19 10:50:28 -0300

    Use base_url for api request

--- src/services/api_service.py
+++ src/services/api_service.py
@@ -3,9 +3,10 @@ import os
 
 import requests
 
+BASE_URL = os.environ["API_BASE_URL"]
 
 def get_key(team_id):
-    url = f"http://localhost:6060/api/organization/get_key/{team_id}"
+    url = f"{BASE_URL}/api/organization/get_key/{team_id}"
     headers = {"X-Shared-Secret": os.environ["API_SHARED_SECRET"]}
     response = requests.get(url=url, headers=headers, timeout=5)
     return response.json()["key"]
