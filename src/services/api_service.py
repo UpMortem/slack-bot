578f6c2421b1a296c9742d869e3dd3e5c9f45cae
fedeq <federicoquattrocchio@gmail.com>
2023-05-22 15:02:00 -0300

    Changed timeout to api calls from 5 to 30 seconds

--- src/services/api_service.py
+++ src/services/api_service.py
@@ -8,5 +8,5 @@ BASE_URL = os.environ["API_BASE_URL"]
 def get_key(team_id):
     url = f"{BASE_URL}/api/organization/get_key/{team_id}"
     headers = {"X-Shared-Secret": os.environ["API_SHARED_SECRET"]}
-    response = requests.get(url=url, headers=headers, timeout=5)
+    response = requests.get(url=url, headers=headers, timeout=30)
     return response.json()["key"]
