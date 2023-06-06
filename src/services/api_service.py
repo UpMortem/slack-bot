ff67275f6a8cc9fe005c5af2df6652ec6552448c
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2023-06-06 11:54:25 -0300

    Improve error handlgin

--- src/services/api_service.py
+++ src/services/api_service.py
@@ -9,4 +9,6 @@ def get_key(team_id):
     headers = {"X-Shared-Secret": os.environ["API_SHARED_SECRET"]}
     response = requests.get(url=url, headers=headers, timeout=30)
     data = response.json()
+    if (data.get("error") is not None):
+        raise Exception(data["error"])
     return {"openai_key": data["openai_key"], "slack_bot_token": data["slack_bot_token"]}
