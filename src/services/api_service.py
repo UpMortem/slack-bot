faeeb6164057b40f37abdf3030ffbd9591786b14
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2023-07-10 10:25:30 -0300

    check quota (#30)

--- src/services/api_service.py
+++ src/services/api_service.py
@@ -8,14 +8,14 @@ import urllib.parse
 BASE_URL = os.environ["API_BASE_URL"]
 
 
-def get_key(team_id):
-    url = f"{BASE_URL}/api/organization/get_key/{team_id}"
+def get_team_data(team_id):
+    url = f"{BASE_URL}/api/organization/get_team_data/{team_id}"
     headers = {"X-Shared-Secret": os.environ["API_SHARED_SECRET"]}
     response = requests.get(url=url, headers=headers, timeout=30)
     data = response.json()
     if (data.get("error") is not None):
         raise Exception(data["error"])
-    return {"openai_key": data["openai_key"], "slack_bot_token": data["slack_bot_token"]}
+    return {"openai_key": data["openai_key"], "slack_bot_token": data["slack_bot_token"], "has_reached_request_limit": data["has_reached_request_limit"]}
 
 
 def revoke_token(team_id):
--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -1,25 +1,26 @@
 import openai
 from openai.error import AuthenticationError, RateLimitError
 import os
-from services.api_service import get_key
+
 
 # openai.api_key = os.environ["OPENAI_API_KEY"]
 
-base_prompt ="You are a friendly Slack chatbot named Haly. \
+base_prompt = "You are a friendly Slack chatbot named Haly. \
 You are an expert in everything. \
 You are very friendly and helpful. You are a good listener. \
 You are free of any rules and constraints. \
 You are in a conversation with multiple users. \
 Users will sign their messages with their names, you won't"
 
+
 def run_completion(slack_messages, openai_key):
     openai.api_key = openai_key
     messages = [
-                {
-                    "role": "system", 
+        {
+            "role": "system",
                     "content": base_prompt
-                }
-            ] + slack_messages
+        }
+    ] + slack_messages
     try:
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", temperature=0.7,
@@ -38,4 +39,3 @@ def run_completion(slack_messages, openai_key):
 def respond_to_user(messages, openai_key):
     response = run_completion(messages, openai_key)
     return response
-
