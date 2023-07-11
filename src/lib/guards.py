cf1315e982886066593ed02e2ee7b28f24014e39
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2023-07-11 13:26:37 -0300

    Track time of some functions

---
+++ src/lib/guards.py
@@ -0,0 +1,14 @@
+import time
+
+
+def time_tracker(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(
+            f"Function '{func.__name__}' took {elapsed_time:.4f} seconds to execute.")
+        return result
+
+    return wrapper
