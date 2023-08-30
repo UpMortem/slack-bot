c5398e01eec56bd97bb0e5410882d37aa435caa9
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2023-08-30 14:59:03 -0300

    Handle app installed event from API (#59)

--- src/lib/guards.py
+++ src/lib/guards.py
@@ -1,5 +1,13 @@
+from functools import wraps
+from http import HTTPStatus
+import os
 import time
 
+from flask import abort, jsonify, request
+
+unauthorized_error = {
+    "message": "Requires authentication"
+}
 
 def time_tracker(func):
     def wrapper(*args, **kwargs):
@@ -12,3 +20,26 @@ def time_tracker(func):
         return result
 
     return wrapper
+
+def shared_secret_guard(function):
+    @wraps(function)
+    def decorator(*args, **kwargs):
+        secret = get_secret_from_request()
+        if secret != os.environ["API_SHARED_SECRET"]:
+            json_abort(HTTPStatus.UNAUTHORIZED, unauthorized_error)
+            return None
+        return function(*args, **kwargs)
+
+    return decorator
+
+def get_secret_from_request():
+    secret = request.headers.get("X-Shared-Secret", None)
+    if not secret:
+        json_abort(HTTPStatus.UNAUTHORIZED, unauthorized_error)
+        return None
+    return secret
+
+def json_abort(status_code, data=None):
+    response = jsonify(data)
+    response.status_code = status_code
+    abort(response)
