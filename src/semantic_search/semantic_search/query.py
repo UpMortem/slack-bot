7d9f66d5592310d5a8d2e1e7000fb1103edd28e9
Yura Lukashik <YuraLukashik@gmail.com>
2023-10-22 22:53:59 +0300

    Search: use one prompt for ChatGPT

--- src/semantic_search/semantic_search/query.py
+++ src/semantic_search/semantic_search/query.py
@@ -1,7 +1,16 @@
+import json
 import logging
 import time
 from .external_services.pinecone import get_pinecone_index
 from .external_services.openai import create_embedding, gpt_query
+
+
+def build_links_list(namespace: str, matches) -> str:
+    if namespace != 'T03QUQ2NFQC':
+        return ""
+    links = map(lambda match: f"https://upmortemworkspace.slack.com/archives/{match['metadata']['channel_id']}"
+                              f"/p{match['metadata']['ts'].replace('.', '')}", matches)
+    return "\n" + "\n".join(links)
 
 
 def smart_query(namespace, query):
@@ -9,7 +18,7 @@ def smart_query(namespace, query):
 
     stage_start_time = time.perf_counter()
     query_vector = create_embedding(query)
-    logging.info(f"Smart Query: embedding created in {round(stage_start_time - time.perf_counter(), 2)}s")
+    logging.info(f"Smart Query: embedding created in {round(time.perf_counter() - stage_start_time, 2)}s")
 
     stage_start_time = time.perf_counter()
     query_results = get_pinecone_index().query(
@@ -19,29 +28,27 @@ def smart_query(namespace, query):
         include_values=False,
         includeMetadata=True
     )
-    logging.info(f"Smart Query: Pinecone search finished in {round(stage_start_time - time.perf_counter(), 2)}s")
+    logging.info(f"Smart Query: Pinecone search finished in {round(time.perf_counter() - stage_start_time, 2)}s")
     query_matches = query_results['results'][0]['matches']
 
-    messages_for_filtering = ["<MSG_ID>" + qm['id'] + '</MSG_ID><MSG>' + qm['metadata']['text'] + "<MSG>" for qm in
-                              query_matches]
-    filter_query = (f"I have a list of messages in the following format: <MSG_ID>Message ID</MSG_ID>"
-                    f"<MSG>Message Text</MSG>. Give me the list of IDs of all messages that are definitely related"
-                    f" to the following search query or may contain an answer to the question in it: \"{query}\". Do not include any explanations, only provide"
-                    f" a list of IDs separated by comma. Prefer more recent messages. Here is the list of messages:")
-    filter_query += "\n" + "\n".join(messages_for_filtering)
+    messages_for_gpt = [{"id": qm["id"], "text": qm["metadata"]["text"]} for qm in query_matches]
+    prompt = ("I have a list of messages in JSON:\n"
+              f"{json.dumps(messages_for_gpt)}\n\n"
+              f"I have the following search query: {query}\n\n"
+              "Give an answer to the query based off the given messages. Only use the messages that are relevant and "
+              "don't make up any answers. Prefer more recent messages.\n"
+              "Be as clear and short as possible and don't use any introductionary words.\n\n"
+              "The output should be a JSON object with the following schema:\n"
+              "{\n"
+              "   \"answer\": \"A string with the answer to the query\",\n"
+              "   \"messages\": [] - an array of IDs (from the JSON given above) from the messages that were used to "
+              "build the answer to the query. Include maximum 3 IDs of the most relevant messages.\n"
+              "}")
 
     stage_start_time = time.perf_counter()
-    filter_response = gpt_query(filter_query)
-    logging.info(f"Smart Query: ChatGPT filtered messages in {round(stage_start_time - time.perf_counter(), 2)}s")
+    result = json.loads(gpt_query(prompt))
+    logging.info(f"Smart Query: Request to ChatGPT took {round(time.perf_counter() - stage_start_time, 2)}s")
 
-    filtered_matches = list(filter(lambda m: m['id'] in filter_response, query_matches))
-    filtered_messages_text = "\n".join([qm['metadata']['text'] for qm in filtered_matches])
+    used_messages = list(filter(lambda match: match["id"] in result["messages"], query_matches))
 
-    summarize_answer_query = (f"Summarize the answer the the following search query: \"{query}\" based off the "
-                              f"information in the found messages: {filtered_messages_text}. Use messages that are "
-                              f"relevant to the query, but don't include anything about that in your answer. Also give "
-                              f"me an explanation of why each of the included messages is relevant.")
-    stage_start_time = time.perf_counter()
-    response = gpt_query(summarize_answer_query)
-    logging.info(f"Smart Query: ChatGPT summarized the answer in {round(stage_start_time - time.perf_counter(), 2)}s")
-    return response
+    return f"{result['answer']}{build_links_list(namespace, used_messages)}"
