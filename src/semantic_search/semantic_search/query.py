6b0e6e05e168cfcba97276a3ef2f91ae337398b4
Yura Lukashik <YuraLukashik@gmail.com>
2023-10-25 19:08:45 +0300

    Search: replace request to Pinecone with a test stub

--- src/semantic_search/semantic_search/query.py
+++ src/semantic_search/semantic_search/query.py
@@ -2,7 +2,7 @@ import json
 import logging
 import time
 from datetime import date
-from .external_services.pinecone import get_pinecone_index
+from .external_services.pinecone import get_pinecone_index, test_pinecone_request
 from .external_services.openai import create_embedding, gpt_query
 
 
@@ -36,15 +36,16 @@ def smart_query(namespace, query, username: str):
     logging.info(f"Smart Query: embedding created in {round(time.perf_counter() - stage_start_time, 2)}s")
 
     stage_start_time = time.perf_counter()
-    query_results = get_pinecone_index().query(
-        queries=[query_vector],
-        top_k=50,
-        namespace=namespace,
-        include_values=False,
-        includeMetadata=True
-    )
+    # query_results = get_pinecone_index().query(
+    #     queries=[query_vector],
+    #     top_k=50,
+    #     namespace=namespace,
+    #     include_values=False,
+    #     includeMetadata=True
+    # )
+    test_pinecone_request()
     logging.info(f"Smart Query: Pinecone search finished in {round(time.perf_counter() - stage_start_time, 2)}s")
-    query_matches = query_results['results'][0]['matches']
+    query_matches = [] # query_results['results'][0]['matches']
 
     messages_for_gpt = [
         {
