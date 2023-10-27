2f97c3b0fb96395f792c5fe334d65522950f2a5e
Yura Lukashik <YuraLukashik@gmail.com>
2023-10-27 14:27:26 +0300

    Search: set log_level=DEBUG for Pinecone loggers

--- src/semantic_search/semantic_search/external_services/pinecone.py
+++ src/semantic_search/semantic_search/external_services/pinecone.py
@@ -1,7 +1,15 @@
+import logging
+
 import pinecone
 from ..config import get_pinecone_key, get_pinecone_environment, get_pinecone_index_name
 
-pinecone.init(api_key=get_pinecone_key(), environment=get_pinecone_environment())
+pinecone.init(api_key=get_pinecone_key(), environment=get_pinecone_environment(), log_level="debug")
+logging.getLogger("pinecone").setLevel("DEBUG")
+logging.getLogger("urllib3").setLevel("DEBUG")
+logging.getLogger("pinecone.core.client").setLevel("DEBUG")
+logging.getLogger("pinecone.core.grpc.retry").setLevel("DEBUG")
+logging.getLogger("pinecone.core.grpc.index_grpc").setLevel("DEBUG")
+logging.getLogger("pinecone.core.client.rest").setLevel("DEBUG")
 
 
 def get_pinecone_index() -> 'pinecone.Index':
