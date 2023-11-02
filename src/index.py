d48d175b3e1a622b163a533124cf5a5579bb01ab
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2023-11-02 13:45:25 -0300

    Migrate to fastApi (#154)

--- src/index.py
+++ src/index.py
@@ -1,34 +1,44 @@
+from fastapi import FastAPI, Request, BackgroundTasks
+from pydantic import BaseModel
 import os
 import logging
-from flask import Flask, request
-from slack_bolt.adapter.flask import SlackRequestHandler
-from lib.guards import shared_secret_guard
-from semantic_search.semantic_search.external_services import openai
+from slack_bolt.adapter.fastapi import SlackRequestHandler
+from slack_bolt import App
+from fastapi.responses import JSONResponse
 from semantic_search.semantic_search.query import smart_query
 from services.slack_service import slack_app, handle_app_installed
 
-logging.basicConfig(level=os.environ["LOG_LEVEL"])
+
+# Configuration and Logging
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
+logging.basicConfig(level=LOG_LEVEL)
+
+# Initialize FastAPI app and Slack Bolt App
+api = FastAPI()
+app_handler = SlackRequestHandler(slack_app)
+
+# Routes
+@api.post("/slack/events")
+async def slack_events_endpoint(req: Request):
+    return await app_handler.handle(req)
+
+@api.post("/slack/app-installed")
+async def app_installed_route(req: Request):
+    handle_app_installed(req)
+    return JSONResponse(content={"message": "App installed successfully"})
+
+# Pydantic model for smart query requests
+class SmartQueryRequest(BaseModel):
+    query: str
+    name: str
+@api.post("/test-smart-query")
+async def test_smart_query(req: SmartQueryRequest):
+    # Using Pydantic model for automatic request parsing and validation
+    # ... code for smart query ...
+    return {"response": smart_query("T03QUQ2NFQC", req.query, req.name)}
 
 
-flask_app = Flask("Haly")
-handler = SlackRequestHandler(slack_app)
-
-@flask_app.route("/slack/events", methods=["POST"])
-def slack_events():
-    return handler.handle(request)
-
-@flask_app.route("/slack/app-installed", methods=["POST"])
-@shared_secret_guard
-def app_installed_route():
-    return handle_app_installed(request)
-
-@flask_app.route("/test-smart-query", methods=["POST"])
-def test_smart_query():
-    return {
-        "response": smart_query("T03QUQ2NFQC", request.json["query"], request.json["name"])
-    }
-
-
+# Running the application with Uvicorn
 if __name__ == "__main__":
-    openai.bootstrap()
-    flask_app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    import uvicorn
+    uvicorn.run(api, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
