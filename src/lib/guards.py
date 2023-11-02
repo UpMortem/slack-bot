a16fb2cf73360d97a256ddf98c0d6489821dfcee
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2023-11-02 13:40:13 -0300

    Migrate to fastApi

--- src/lib/guards.py
+++ src/lib/guards.py
@@ -1,45 +1,34 @@
 from functools import wraps
-from http import HTTPStatus
+from fastapi import HTTPException, Request, status
+from fastapi.responses import JSONResponse
 import os
 import time
-
-from flask import abort, jsonify, request
 
 unauthorized_error = {
     "message": "Requires authentication"
 }
 
 def time_tracker(func):
-    def wrapper(*args, **kwargs):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
-        result = func(*args, **kwargs)
+        result = await func(*args, **kwargs)  # Assuming this can be an async function
         end_time = time.perf_counter()
         elapsed_time = end_time - start_time
-        print(
-            f"Function '{func.__name__}' took {elapsed_time:.4f} seconds to execute.")
+        print(f"Function '{func.__name__}' took {elapsed_time:.4f} seconds to execute.")
         return result
-
     return wrapper
 
-def shared_secret_guard(function):
-    @wraps(function)
-    def decorator(*args, **kwargs):
-        secret = get_secret_from_request()
-        if secret != os.environ["API_SHARED_SECRET"]:
-            json_abort(HTTPStatus.UNAUTHORIZED, unauthorized_error)
-            return None
-        return function(*args, **kwargs)
+def shared_secret_guard(request: Request):
+    secret = get_secret_from_request(request)
+    if secret != os.environ["API_SHARED_SECRET"]:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=unauthorized_error)
 
-    return decorator
+    # If the secret matches, simply return True or some other success indicator
+    return True
 
-def get_secret_from_request():
-    secret = request.headers.get("X-Shared-Secret", None)
+def get_secret_from_request(request: Request):
+    secret = request.headers.get("X-Shared-Secret")
     if not secret:
-        json_abort(HTTPStatus.UNAUTHORIZED, unauthorized_error)
-        return None
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=unauthorized_error)
     return secret
-
-def json_abort(status_code, data=None):
-    response = jsonify(data)
-    response.status_code = status_code
-    abort(response)
